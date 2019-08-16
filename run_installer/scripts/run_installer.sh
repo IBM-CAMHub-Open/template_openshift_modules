afe7b998d0ea8d37e94244a57b652dd46f6f6632
CAMHub PublishBot <octravis@us.ibm.com>
2019-08-16 12:33:33 +0000

    Automated CAMHub::publish on 2019-08-16 12:33:33 UTC

--- run_installer/scripts/run_installer.sh
+++ run_installer/scripts/run_installer.sh
@@ -8,17 +8,32 @@ if ansible-playbook -vvv playbooks/prerequisites.yml ; then
     printf "\033[32m[*] Prerequisites Succeeded \033[0m\n"
     if ansible-playbook -vvv playbooks/deploy_cluster.yml ; then
         printf "\033[32m[*] Deploy OpenShift Cluster Succeeded \033[0m\n"
-        #sed -i -e 's/DenyAllPasswordIdentityProvider/HTPasswdPasswordIdentityProvider/g' /etc/origin/master/master-config.yaml
-        #sed -i -e '/HTPasswdPasswordIdentityProvider/a \ \ \ \ \ \ file: /etc/origin/master/htpasswd' /etc/origin/master/master-config.yaml
-        #sed -ni '/provider:/{x;d;};1h;1!{x;p;};${x;p;}' /etc/origin/master/master-config.yaml
-        #sed -i -e '/provider:/i \ \ \ \ name: htpasswd_auth provider' /etc/origin/master/master-config.yaml
         yum -y install httpd-tools
         touch /etc/origin/master/htpasswd
         htpasswd -b /etc/origin/master/htpasswd $1 $2
+        # restart openshift
         master-restart api
         master-restart controllers
+        for i in {1..10}
+        do
+            response=`curl -o /dev/null -s -w "%{http_code}" -k https://$(hostname -f):8443`
+            if [ $response != 200 ]; then
+                if [[ $i == 10 ]]; then
+                    printf "\033[31m[ERROR] Unable to connect to the OCP server after 20 minutes\033[0m\n"
+                else
+                    echo "Login response is ${response}. Wait for OCP to be available ..."
+                    sleep 120
+                fi
+            else
+                echo "Login response is ${response}. OCP is available now ..."
+                break
+            fi
+        done
+        echo "Logging in as system:admin"
         oc login -u system:admin -n default
+        echo "Successfully logged in as system:admin. Assigning cluster-admin role to $1 user"
         oc adm policy add-cluster-role-to-user cluster-admin $1
+        echo "Successfully assigned cluster-admin role to $1 user"
         #oc project default
         printf "\033[32m[*] User Account Created Successfully \033[0m\n"
     else
